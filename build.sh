49e36b000f25e12a26e2683cde48d7c786ea4146
Renan.dev <dev.rfcsb@gmail.com>
2023-09-23 11:42:46 -0400

    att

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 python manage.py migrate
