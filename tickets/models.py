e3df9c5150b038e4d0ed7c38e751245f93ef9a68
Renan.dev <dev.rfcsb@gmail.com>
2023-09-23 11:29:43 -0400

    att

--- tickets/models.py
+++ tickets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 
 
@@ -8,18 +9,21 @@ class Ticket(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
     code = models.CharField(max_length=127)
     
-    # user = models.ForeignKey(
-    #     "users.User", on_delete=models.CASCADE, related_name="ticket"
-    # )
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="ticket"
+    )
+    orders = models.ManyToManyField(
+        "users.User", through="tickets.TicketsOrder", related_name="ticket_order"
+    )
 
 
-# class TicketsOrder(models.Model):
-#     buyed_at = models.DateTimeField(auto_now_add=True)
-#     price = models.DecimalField(max_digits=8, decimal_places=2)
-#     code = models.CharField(max_length=127)
-#     ticket = models.ForeignKey(
-#         "tickets.Ticket", on_delete=models.CASCADE, related_name="order_ticket"
-#     )
-#     user = models.ForeignKey(
-#         "users.User", on_delete=models.CASCADE, related_name="user_ticket"
-#     )
+class TicketsOrder(models.Model):
+    buyed_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    code = models.CharField(max_length=127)
+    ticket = models.ForeignKey(
+        "tickets.Ticket", on_delete=models.CASCADE, related_name="order_ticket"
+    )
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="user_ticket"
+    )
