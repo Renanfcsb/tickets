82e1ea95f0e017a4c463e88eadc74302ce8136a7
Renan.dev <dev.rfcsb@gmail.com>
2023-09-27 22:52:23 -0400

    att

--- _tickets/settings.py
+++ _tickets/settings.py
@@ -44,16 +44,16 @@ THIRD_PARTY_APPS = ["rest_framework"]
 
 MY_APPS = ["users", "tickets"]
 DJANGO_APPS = [
-    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
-INSTALLED_APPS = THIRD_PARTY_APPS + DJANGO_APPS + MY_APPS
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
